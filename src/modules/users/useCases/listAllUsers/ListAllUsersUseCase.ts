24c7b147d2732b0fbe8c1fa7710245fa75c9c8d9
weyne <weyneace@gmail.com>
2023-02-10 00:16:18 -0300

    Create ListAllUsersUseCase.ts

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,23 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const foundUser = this.usersRepository.findById(user_id);
+    if (!foundUser) {
+      throw new Error("User not found");
+    }
+    if (!foundUser.admin) {
+      throw new Error("User provided is not admin");
+    }
+    return this.usersRepository.list();
+  }
+}
+
+export { ListAllUsersUseCase };
